f4775e0972fe00b64f65f370c2ae5098ccb67e88
GinoGreen <luigiverde97@gmail.com>
2021-11-24 17:42:01 +0100

    Shortcut responsive

--- src/components/Shortcut.vue
+++ src/components/Shortcut.vue
@@ -5,23 +5,23 @@
 
          <div class="row">
 
-            <div class="thumb col-lg-2 col-md-3 offset-lg-1 offset-3">
+            <div class="thumb col-lg-2 col-md-3 col-sm-6 offset-lg-1 offset-md-2 offset-sm-0">
                <img src="../assets/img/buy-comics-digital-comics.png" alt="digital comics">
                <h4>digital comics</h4>
             </div>
-            <div class="thumb col-lg-2 col-md-3">
+            <div class="thumb col-lg-2 col-md-3 col-sm-6">
                <img src="../assets/img/buy-comics-merchandise.png" alt="digital comics">
                <h4>dc merchandise</h4>
             </div>
-            <div class="thumb col-lg-2 col-md-3">
+            <div class="thumb col-lg-2 col-md-3 col-sm-6">
                <img src="../assets/img/buy-comics-shop-locator.png" alt="digital comics">
                <h4>comic shop locator</h4>
             </div>
-            <div class="thumb col-lg-2 col-md-3 offset-lg-0 offset-md-3">
+            <div class="thumb col-lg-2 col-md-3 col-sm-6 offset-lg-0 offset-md-3 offset-sm-0">
                <img src="../assets/img/buy-comics-subscriptions.png" alt="digital comics">
                <h4>subscription</h4>
             </div>
-            <div class="thumb col-lg-2 col-md-3">
+            <div class="thumb col-lg-2 col-md-3 col-sm-6 offset-md-0 offset-sm-3">
                <img src="../assets/img/buy-dc-power-visa.svg" alt="digital comics">
                <h4>dc power visa</h4>
             </div>
