ac2db54e4117762b729047edec3709d45e0b17ac
GinoGreen <luigiverde97@gmail.com>
2021-11-24 18:02:23 +0100

    background footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,22 @@
 <template>
    <footer>
-      
+
+      <div class="lists">
+         <div class="container bg-dc-logo">
+         
+
+
+         </div>
+      </div>
+
+      <div class="bottom-footer">
+         <div class="container">
+
+
+
+         </div>
+      </div>
+
    </footer>
 </template>
 
@@ -11,5 +27,23 @@ export default {
 </script>
 
 <style lang="scss">
+   @import '../assets/style/mixins.scss';
+   @import '../assets/style/vars.scss';
+
+   footer .lists {
+      background-image: url('../assets/img/footer-bg.jpg');
+
+      .bg-dc-logo {
+         padding: 40px 0;
+         background-image: url('../assets/img/dc-logo-bg.png');
+         background-position: right;
+         background-repeat: no-repeat;
+      }
+   }
+
+   footer .bottom-footer {
+      height: 20px;
+      background-color: #303030;
+   }
 
 </style>
