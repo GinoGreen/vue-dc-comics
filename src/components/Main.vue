e314470757873b0f84dc69169b43c83f6a88dc88
GinoGreen <luigiverde97@gmail.com>
2021-11-25 12:40:41 +0100

    making cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,36 @@
 <template>
    <main>
-      
+
+      <section id="jumbotrone"></section>
+
+      <section id="thumbs" class="container">
+         <TemplateCard
+            v-for="(card, index) in Cards"
+            :key="index"
+            :card="card"
+         />
+      </section>
+
+      <button>load more</button>
+
    </main>
 </template>
 
 <script>
+
+import Cards from '../assets/data/Cards';
+import TemplateCard from './TemplateCard';
+
 export default {
-   name: 'Main'
+   name: 'Main',
+   components: {
+      TemplateCard
+   },
+   data() {
+      return {
+         Cards
+      }
+   }
 }
 </script>
 
@@ -15,7 +39,26 @@ export default {
    @import '../assets/style/vars.scss';
 
    main {
-      padding: 90px 0;
+      padding: 20px 0;
       background-color: #1C1C1C;
+      @include center();
+      flex-direction: column;
+
+      #thumbs {
+         display: flex;
+         flex-wrap: wrap;
+      }
+
+      button {
+         padding: 8px 50px;
+
+         color: #fff;
+         font-size: 1rem;
+         font-weight: bold;
+         text-transform: uppercase;
+
+         border: none;
+         background-color: $brand_color;
+      }
    }
 </style>
--- src/components/TemplateCard.vue
+++ src/components/TemplateCard.vue
@@ -1,13 +1,44 @@
 <template>
-   <div></div>
+   <div class="myCard">
+      <img :src="card.thumb" :alt="card.series">
+      <!-- <h5>{{ card.series }}</h5> -->
+   </div>
 </template>
 
 <script>
 export default {
-   name: 'TemplateCard'
+   name: 'TemplateCard',
+   props: {
+      card: Object
+   }
 }
 </script>
 
-<style>
+<style lang="scss">
+   @import '../assets/style/mixins.scss';
+   @import '../assets/style/vars.scss';
+
+   .myCard {
+      width: calc(100% / 6 - 40px);
+      margin: 0 20px;
+
+      cursor: pointer;
+
+      img {
+         width: 100%;
+         // height: calc(50%);
+
+         display: block;
+
+         object-fit: cover;
+         object-position: top;
+      }
+
+      h5 {
+         margin-top: 20px;
+         font-size: .9rem;
+         color: #fff;
+      }
+   }
 
 </style>
